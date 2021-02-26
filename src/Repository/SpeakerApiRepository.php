ad0ef6890c6d091921ac0b403408723f3c2f835d
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 23:15:21 +0000

    Add SpeakerApiRepository concrete class

---
+++ src/Repository/SpeakerApiRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+final class SpeakerApiRepository implements SpeakerRepository
+{
+    public function findAll(): array
+    {
+        return [];
+    }
+}
