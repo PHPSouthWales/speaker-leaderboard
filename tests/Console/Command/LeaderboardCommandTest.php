9444c5b707c7ed567e6cc9c3e1e9ab69fb2a83d2
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 22:07:29 +0000

    Create and execute the CommandTester

--- tests/Console/Command/LeaderboardCommandTest.php
+++ tests/Console/Command/LeaderboardCommandTest.php
@@ -7,6 +7,7 @@ namespace App\Tests\Console\Command;
 use App\Console\Command\LeaderboardCommand;
 use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Console\Tester\CommandTester;
 
 final class LeaderboardCommandTest extends WebTestCase
 {
@@ -14,6 +15,9 @@ final class LeaderboardCommandTest extends WebTestCase
     public function it_generates_a_leaderboard_of_speakers(): void
     {
         $command = $this->getContainer()->get(LeaderboardCommand::class);
+        $commandTester = new CommandTester($command);
+
+        $commandTester->execute([]);
     }
 
     private function getContainer(): ContainerInterface
