95680540af52389d3221896f610fe5c4caf997be
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 22:08:56 +0000

    Override the execute method

--- src/Console/Command/LeaderboardCommand.php
+++ src/Console/Command/LeaderboardCommand.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace App\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class LeaderboardCommand extends Command
 {
-
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return 0;
+    }
 }
