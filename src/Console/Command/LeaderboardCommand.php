61c6b33a6afd5f37034078323e3e26585dff9a87
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 22:54:36 +0000

    Inject a speaker repository

--- src/Console/Command/LeaderboardCommand.php
+++ src/Console/Command/LeaderboardCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Console\Command;
 
+use App\Repository\SpeakerRepository;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,6 +12,15 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 final class LeaderboardCommand extends Command
 {
+    protected static $defaultName = 'php-south-wales:generate-leaderboard';
+
+    public function __construct(
+        string $name = null,
+        private SpeakerRepository $speakerRepository
+    ) {
+        parent::__construct($name);
+    }
+
     public function execute(InputInterface $input, OutputInterface $output): int
     {
         $table = new Table($output);
