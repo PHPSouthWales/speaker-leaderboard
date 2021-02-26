53f5f612f8446de3348ae282eb79385d48877d4c
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 23:43:32 +0000

    Inject the HTTP client

--- src/Repository/SpeakerApiRepository.php
+++ src/Repository/SpeakerApiRepository.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Tightenco\Collect\Support\Collection;
 
 final class SpeakerApiRepository implements SpeakerRepository
 {
+    public function __construct(
+        private HttpClientInterface $client
+    ) {}
+
     public function findAll(): Collection
     {
         return new Collection();
