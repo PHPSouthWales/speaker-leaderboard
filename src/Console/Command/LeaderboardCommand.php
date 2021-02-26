b91c576e1f3b3e527ac04ba08791a3011dfb39b6
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 22:25:20 +0000

    Add and use the table helper

--- src/Console/Command/LeaderboardCommand.php
+++ src/Console/Command/LeaderboardCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -12,6 +13,10 @@ final class LeaderboardCommand extends Command
 {
     public function execute(InputInterface $input, OutputInterface $output): int
     {
+        $table = new Table($output);
+        $table->setHeaders(['Speaker name', 'Number of talks']);
+        $table->render();
+
         return 0;
     }
 }
