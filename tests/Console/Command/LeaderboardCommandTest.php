0f2f7dfb61b955c498ba8e0fa41d50968c1ed7b9
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 21:50:33 +0000

    Scaffold a new test

---
+++ tests/Console/Command/LeaderboardCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Console\Command;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class LeaderboardCommandTest extends WebTestCase
+{
+    /** @test */
+    public function it_generates_a_leaderboard_of_speakers(): void
+    {
+    }
+}
