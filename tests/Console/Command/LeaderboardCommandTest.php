2864a8080fe0ffe2ebf6e1dad2574ca6e6cf21bb
Oliver Davies <oliver@oliverdavies.uk>
2021-02-26 21:55:25 +0000

    Get access to the container

--- tests/Console/Command/LeaderboardCommandTest.php
+++ tests/Console/Command/LeaderboardCommandTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Tests\Console\Command;
 
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 final class LeaderboardCommandTest extends WebTestCase
@@ -11,5 +12,12 @@ final class LeaderboardCommandTest extends WebTestCase
     /** @test */
     public function it_generates_a_leaderboard_of_speakers(): void
     {
+    }
+
+    private function getContainer(): ContainerInterface
+    {
+        $this->bootKernel();
+
+        return self::$container;
     }
 }
