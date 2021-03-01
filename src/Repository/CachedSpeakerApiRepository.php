281f8fcb83c55cb21c3c2ff2ea210c6c48950edf
Oliver Davies <oliver@oliverdavies.uk>
2021-03-01 12:00:00 +0000

    Get the speaker info from the cache

--- src/Repository/CachedSpeakerApiRepository.php
+++ src/Repository/CachedSpeakerApiRepository.php
@@ -4,16 +4,23 @@ declare(strict_types=1);
 
 namespace App\Repository;
 
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 use Tightenco\Collect\Support\Collection;
 
 final class CachedSpeakerApiRepository implements SpeakerRepository
 {
     public function __construct(
+        private CacheInterface $cache,
         private SpeakerApiRepository $speakerRepository,
     ) {}
 
     public function findAll(): Collection
     {
-        return $this->speakerRepository->findAll();
+        return $this->cache->get('api.speaker_info', function (ItemInterface $item): Collection {
+            $item->expiresAt(date_create('tomorrow'));
+
+            return $this->speakerRepository->findAll();
+        });
     }
 }
